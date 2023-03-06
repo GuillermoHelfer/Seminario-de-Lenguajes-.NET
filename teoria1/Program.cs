2dff41c12492a23d980a521e9e58e4a1c7aa1dbf
GuillermoHelfer <89919447+GuillermoHelfer@users.noreply.github.com>
2023-03-06 16:25:35 -0300

    Initial commit

---
+++ teoria1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//tarea 1; diferente resultado por diferente tipo, int div int = int; int div real = real.
+double r1 = 17 / 3;
+double r2 = 17 / 3.0;
+Console.WriteLine(r1);
+Console.WriteLine(r2);
+//tarea2; imprime 4 true, si se le asigna a x el valor 0 hay error en div por 0.
+int x = 2;
+int y = 5;
+Console.WriteLine(x != 0 && y / x == 2);
+Console.WriteLine(x != 0 & y / x == 2);
+Console.WriteLine(x == 0 || y / x == 2);
+Console.WriteLine(x == 0 | y / x == 2);
+//tarea 3; post incremento primero imprime y luego suma, pre incremento primero suma luego imprime
+x = 1;
+Console.WriteLine(x++); //post incremento
+Console.WriteLine(x);
+Console.WriteLine(++x); //pre incremento
+Console.WriteLine(x);
+//tarea 4; post decremento primero se asigna el valor de lla variable y luego resta a la variable original, post es viceversa.
+x = 10;
+y = x--; //post decremento
+Console.WriteLine(y);
+Console.WriteLine(x);
+y = --x; //pre decremento
+Console.WriteLine(y);
+Console.WriteLine(x);
+//tarea 5; post = primero compara luego incrementa/decrementa, pre primero incrementa/decrementa y luego compara
+x = 10;
+Console.WriteLine(x++ == 10);
+Console.WriteLine(x-- == 10);
+Console.WriteLine(++x == 10);
+Console.WriteLine(--x == 10);
