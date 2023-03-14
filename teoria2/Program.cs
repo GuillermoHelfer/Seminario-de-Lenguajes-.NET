4c254d7dabb531e53e72c74c63889d1e933367d6
GuillermoHelfer <89919447+GuillermoHelfer@users.noreply.github.com>
2023-03-14 16:07:05 -0300

    clase 2

--- teoria2/Program.cs
+++ teoria2/Program.cs
@@ -4,3 +4,56 @@ obj = "casa";                               // ahora de tipo string
 Console.WriteLine(obj);
 obj = 4;                                    // ahora de tipo int
 Console.WriteLine(obj);
+
+////////////////////////
+Console.WriteLine("/////////////////////////////");
+////////////////////////
+
+obj = 1 + 2.3;
+
+Console.WriteLine(obj.GetType());
+Console.WriteLine(obj);
+
+////////////////////////
+Console.WriteLine("/////////////////////////////");
+////////////////////////
+
+obj = 1 + 'A';
+
+Console.WriteLine(obj);
+
+////////////////////////
+Console.WriteLine("/////////////////////////////");
+////////////////////////
+
+obj = "CAS" + 'A';
+
+Console.WriteLine(obj);
+
+////////////////////////
+Console.WriteLine("/////////////////////////////");
+////////////////////////
+
+obj = "AREA" + 51;
+
+Console.WriteLine(obj.GetType());
+Console.WriteLine(obj);
+
+////////////////////////
+Console.WriteLine("/////////////////////////////");
+////////////////////////
+
+Console.WriteLine("Area" + 5 + 1);
+Console.WriteLine(5 + 1 + "Area");
+
+////////////////////////
+Console.WriteLine("/////////////////////////////");
+////////////////////////
+
+char c1 = 'A';
+char c2 = 'A';
+Console.WriteLine(c1 == c2);
+
+object o1 = 'A';
+object o2 = 'A';
+Console.WriteLine(o1 == o2);
