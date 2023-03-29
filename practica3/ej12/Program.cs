8460ba8d6c95319211bba4da4bfc2cfcdac247dd
GuillermoHelfer <89919447+GuillermoHelfer@users.noreply.github.com>
2023-03-28 23:45:25 -0300

    mensajecito

--- practica3/ej12/Program.cs
+++ practica3/ej12/Program.cs
@@ -3,26 +3,70 @@
 Console.WriteLine("ingrese la frase a encriptar");
 String frase = Console.ReadLine();
 
-StringBuilder key = new StringBuilder();
+
 Console.WriteLine("ingrese la clave de cifrado");
-key.Append(Console.ReadLine());
-Queue<char> q = new Queue<char>();
+string key = Console.ReadLine();
+Queue<int> q = new Queue<int>();
 for(int i=0; i<key.Length; i++){
-    q.Enqueue()
+    q.Enqueue((int)key[i] - 48);
 }
 
 Dictionary<char,int> dct = new Dictionary<char,int>();
 
-for (int i=1, j= 65; i< 28; i++, j++)
+for (int i=1, j= 65; j<= 90; i++, j++)
     dct.Add((char)j, i);
-dct.Add((char)32, 28);
+dct.Add((char)32, 27);
 
-StringBuilder cifrar (string frase, Queue<char> kq, Dictionary<char,int> dct)
+string st = cifrar(frase,ref q, dct).ToString();
+Console.WriteLine(st);
+q.Clear();
+for(int i=0; i<key.Length; i++){
+    q.Enqueue((int)key[i] - 48);
+}
+Console.WriteLine(decifrar(st,ref q, dct));
+Console.ReadKey(true);
+
+StringBuilder cifrar (string frase, ref Queue<int> kq, Dictionary<char,int> dct)
 {
-    for (int i=0; i<=28; i++)
+    StringBuilder cifrada = new StringBuilder();
+    int qValue;
+    for (int i=0; i<frase.Length; i++)
     {
-        
+        qValue = kq.Dequeue();
+        dct[frase[i]]+= qValue;
+        if (dct[frase[i]] > 27)
+            dct[frase[i]] -= 27;
+        char value = (char)(dct[frase[i]]+ 64);
+        cifrada.Append(value);
+        dct[frase[i]]-= qValue;
+        kq.Enqueue(qValue);
     }
-    return null;
+    return cifrada;
 }
 
+StringBuilder decifrar (string frase, ref Queue<int> kq, Dictionary<char,int> dct)
+{
+    StringBuilder cifrada = new StringBuilder();
+    int qValue;
+    for (int i=0; i<frase.Length; i++)
+    {
+        qValue = kq.Dequeue();
+        dct[frase[i]]-= qValue;
+        char value;
+        if (dct[frase[i]] == 0)
+        {
+            dct[frase[i]] = 32;
+            value = (char)(dct[frase[i]]);
+        }
+        else
+        {
+            if (dct[frase[i]] < 0)
+                dct[frase[i]] += 27;
+            value = (char)(dct[frase[i]]+ 64);
+        }
+        cifrada.Append(value);
+        dct[frase[i]]+= qValue;
+        kq.Enqueue(qValue);
+    }
+    return cifrada;
+}
